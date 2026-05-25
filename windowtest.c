1a58bcfdeb1b65aee51bfc581c71be0f698e6c1b
Sunshine <yewhang2004@gmail.com>
2026-05-25 17:14:11 +0800

    Konpeko konpeko konpeko~.

--- windowtest.c
+++ windowtest.c
@@ -18,12 +18,77 @@ int MessageBoxPrintf(TCHAR* szCaption, TCHAR* szFormat, ...){
     return MessageBox(NULL, szBuffer, szCaption, 0);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow){
-    int cxScreen, cyScreen;
-    cxScreen = GetSystemMetrics(SM_CXSCREEN);
-    cyScreen = GetSystemMetrics(SM_CYSCREEN);
+LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM);
 
-    MessageBoxPrintf(TEXT("uwu screen size"), TEXT("The screen is %i pixels wide by %i pixels high."), cxScreen, cyScreen);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow){
+    static TCHAR szAppName[] = TEXT("Hewwo pookie");
+    HWND hwnd;
+    MSG msg;
+    WNDCLASS wndclass = {
+        .style = CS_HREDRAW | CS_VREDRAW,
+        .lpfnWndProc = WndProc,
+        .cbClsExtra = 0,
+        .cbWndExtra = 0,
+        .hInstance = hInstance,
+        .hIcon = LoadIcon(NULL, IDI_APPLICATION),
+        .hCursor = LoadCursor(NULL, IDC_ARROW),
+        .hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH),
+        .lpszMenuName = NULL,
+        .lpszClassName = szAppName
+    };
+
+    if (!RegisterClass(&wndclass)){
+        MessageBox(NULL, TEXT("My guy it is time for you to change your computer"), szAppName, MB_ICONERROR);
+
+        return 0;
+    }
+
+    hwnd = CreateWindow(
+        szAppName,
+        TEXT("window time uwu"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0)){
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
     
-    return 0;
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rect;
+
+    switch (message){
+        case WM_CREATE:
+            PlaySound(TEXT("konpeko.wav"), NULL, SND_FILENAME | SND_ASYNC);
+            return 0;
+        
+        case WM_PAINT:
+            hdc = BeginPaint(hwnd, &ps);
+            GetClientRect(hwnd, &rect);
+            DrawText(hdc, TEXT("Konpeko~"), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            EndPaint(hwnd, &ps);
+            return 0;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+
+    return DefWindowProc(hwnd, message, wParam, lParam);
 }
