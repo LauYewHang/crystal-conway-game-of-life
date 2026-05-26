d150f7fbdc6816958c332fc6723ad26b04778b73
Sunshine <yewhang2004@gmail.com>
2026-05-26 09:59:09 +0800

    Added comment for reference.

--- windowtest.c
+++ windowtest.c
@@ -26,18 +26,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
     HWND hwnd; // handle to a window
     MSG msg; // message structure
     WNDCLASS wndclass = { // window class structure
-        .style = CS_HREDRAW | CS_VREDRAW,
-        .lpfnWndProc = WndProc,
-        .cbClsExtra = 0,
-        .cbWndExtra = 0,
-        .hInstance = hInstance, // HINSTANCE: handle to an "instance" of the program itself
+        .style = CS_HREDRAW | CS_VREDRAW, // define the window class style; in this case it defines the window should repaint itself whenever the horizontal (CS_HREDRAW) or vertical (CS_VREDRAW) window size changes
+        .lpfnWndProc = WndProc, // address of window procedure; a callback function of type `LRESULT CALLBACK Wndproc(HWMD, UINT, WPARAM, LPARAM)`
+        .cbClsExtra = 0, // number of extra bytes to allocate for the window class structure
+        .cbWndExtra = 0, // number of extra bytes to allocate for the window instance
+        .hInstance = hInstance, // the instance handle of the program; HINSTANCE: handle to an "instance" of the program itself
         .hIcon = LoadIcon(NULL, IDI_APPLICATION), // load an icon to display at the program title bar
         .hCursor = LoadCursor(NULL, IDC_HAND), // load a mouse cursor (style) for the program; the style is something like "Select", "Person", "Unavailable" (refer to your fox cursor folder); it specified how the cursor should be presented in your program
         .hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH), // obtain a graphic object; in this case a brush for painting window's background
-        .lpszMenuName = NULL,
-        .lpszClassName = szAppName
+        .lpszMenuName = NULL, // specify the window class menu; in this case, there is no application menu, thus NULL
+        .lpszClassName = szAppName // text name of the window class (can be whatever you want)
     };
 
+    // register the window class
+    // idea of !RegisterClass is that the older version of Windows (Windows 98) doesn't have function for Unicode char,
+    // thus the RegisterClass that is trying to call RegisterClassW will run into error,
+    // and if an error occur, we notify the user by messagebox that their device does not support Unicode (doesn't have unicode related .dll)
     if (!RegisterClass(&wndclass)){
         MessageBox(NULL, TEXT("My guy it is time for you to change your computer"), szAppName, MB_ICONERROR);
 
@@ -46,17 +50,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 
     // create a window based on a window class
     hwnd = CreateWindow(
-        szAppName,
-        TEXT("window time uwu"),
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
+        szAppName, // window class name
+        TEXT("window time uwu"), // window caption
+        WS_OVERLAPPEDWINDOW, // window style
+        CW_USEDEFAULT, // initial x position
+        CW_USEDEFAULT, // initial y position
+        CW_USEDEFAULT, // initial x size
+        CW_USEDEFAULT, // initial y size
+        NULL, // parent window handle
+        NULL, // window menu handle
+        hInstance, // program instance handle
+        NULL // creation parameters
     );
 
     ShowWindow(hwnd, iCmdShow); // display the window on the screen
@@ -68,7 +72,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
         DispatchMessage(&msg); // send message to window procedure
     }
     
-    return msg.wParam;
+    return msg.wParam; // return the value passed to PostQuitMessage() (usually 0 to indicate the program has successfully terminated)
 }
 
 // CALLBACK is a special calling sequence for function calls that occur between Windows and the application (CALLBACK is defined as __stdcall)
@@ -77,11 +81,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
     PAINTSTRUCT ps; // paint structure
     RECT rect; // rectangle structure
 
+    // switch case to determine the message value
+    // all message processed by window procedure should return 0
     switch (message){
+        // initialization of window (by CreateWindow)
         case WM_CREATE:
             PlaySound(TEXT("konpeko.wav"), NULL, SND_FILENAME | SND_ASYNC); // play a sound file
             return 0;
         
+        // repaint the window; called by UpdateWindow or when the window's client area is invalid (in this case, when window is being resized (due to wndclass.style))
         case WM_PAINT:
             hdc = BeginPaint(hwnd, &ps); // initialize the beginning of window painting
             GetClientRect(hwnd, &rect); // obtain the dimension of the window's client area
@@ -94,5 +102,5 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
             return 0;
     }
 
-    return DefWindowProc(hwnd, message, wParam, lParam); // perform default processing of messages
+    return DefWindowProc(hwnd, message, wParam, lParam); // perform default processing of messages; proess the message that is not processed by window procedure
 }
